c23d3792c01c3fc2ed083e5f9e3c9eba6db36d8f
Claas <claas.rostock@dnvgl.com>
2022-09-26 16:30:37 +0200

    added resetDemo.bat batch files in gunnerus-dp and variable_groups examples

---
+++ tests/gunnerus-dp/resetDemo.bat
@@ -0,0 +1,11 @@
+del *.csv
+del *callGraph*
+del *.fmu
+del *.log
+REM del caseDict
+del caseDict_imported_from_OspSystemStructure_xml
+del OspSystemStructure.xml
+del statisticsDict
+del SystemStructure.ssd
+del watchDict
+rmdir /S /Q results
