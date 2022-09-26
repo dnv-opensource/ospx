16985385e38b818ef56652fbd2cf93637e6a4ebb
Claas <claas.rostock@dnvgl.com>
2022-09-26 22:46:10 +0200

    added tests\test_dicts\resetDemo.bat

---
+++ tests/test_dicts/resetDemo.bat
@@ -0,0 +1,10 @@
+del *.csv
+del *callGraph*
+del *.fmu
+del *.log
+del caseDict_imported_from_test_import_OspSystemStructure_xml
+del OspSystemStructure.xml
+del statisticsDict
+del SystemStructure.ssd
+del watchDict
+rmdir /S /Q results
