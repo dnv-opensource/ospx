e4086859f7d6e359395ec94da7e0acd762204fc2
Claas <claas.rostock@dnv.com>
2024-11-11 18:31:21 +0100

    __init__.py's : re-exported imported names

--- src/ospx/__init__.py
+++ src/ospx/__init__.py
@@ -1,12 +1,12 @@
-from ospx.simulation import Simulation
-from ospx.connector import Connector
+from ospx.simulation import Simulation as Simulation
+from ospx.connector import Connector as Connector
 from ospx.connection import (
-    Endpoint,
-    Connection,
+    Endpoint as Endpoint,
+    Connection as Connection,
 )
-from ospx.component import Component
-from ospx.system import System
-from ospx.ospSimulationCase import OspSimulationCase
-from ospx.graph import Graph
-from ospx.ospCaseBuilder import OspCaseBuilder
-from ospx.importer import OspSystemStructureImporter
+from ospx.component import Component as Component
+from ospx.system import System as System
+from ospx.ospSimulationCase import OspSimulationCase as OspSimulationCase
+from ospx.graph import Graph as Graph
+from ospx.ospCaseBuilder import OspCaseBuilder as OspCaseBuilder
+from ospx.importer import OspSystemStructureImporter as OspSystemStructureImporter
--- src/ospx/fmi/__init__.py
+++ src/ospx/fmi/__init__.py
@@ -1,11 +1,11 @@
-from ospx.fmi.experiment import Experiment
+from ospx.fmi.experiment import Experiment as Experiment
 from ospx.fmi.unit import (
-    Unit,
-    BaseUnit,
-    DisplayUnit,
+    Unit as Unit,
+    BaseUnit as BaseUnit,
+    DisplayUnit as DisplayUnit,
 )
 from ospx.fmi.variable import (
-    ScalarVariable,
-    get_fmi_data_type,
+    ScalarVariable as ScalarVariable,
+    get_fmi_data_type as get_fmi_data_type,
 )
-from ospx.fmi.fmu import FMU
+from ospx.fmi.fmu import FMU as FMU
